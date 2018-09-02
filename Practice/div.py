3aa72493f0411b471d1eccca8ae62a80dac09506
Darius <darius.jones1018@gmail.com>
2018-09-02 12:15:17 -0700

    uploaded div algo

---
+++ Practice/div.py
@@ -0,0 +1,22 @@
+# Darius Jones
+# 9/2/2018
+# Division 
+
+import sys
+
+def div(x,y):
+
+	#check precondtions
+	if x<=0 and y<0 and x != int(x) and y != int(y):
+		raise ValueError("Invalid inputs for Division.")
+
+	#algorithm
+	quotient = 0
+	remainder = x
+
+	while y<= remainder:
+		remainder -= y
+		quotient++
+		
+return quotient, remainder
+
