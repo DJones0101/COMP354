a05932a466b256bc78289460a04602d1b2a8599f
Unknown <darius.jones1018@gmail.com>
2018-09-02 12:46:08 -0700

    Finished up the algo.

--- Practice/div.py
+++ Practice/div.py
@@ -1,6 +1,6 @@
 # Darius Jones
 # 9/2/2018
-# Division 
+# Division 1.1 from the book
 
 import sys
 
@@ -15,8 +15,15 @@ def div(x,y):
 	remainder = x
 
 	while y<= remainder:
-		remainder -= y
-		quotient += 1
-		
-return quotient, remainder
+		remainder = remainder - y
+		quotient = quotient + 1
 
+		print("remainder = " +str(remainder) + " quotient = "+ str(quotient))
+		
+	return quotient, remainder
+
+# As per the book the (post condition/ loop invariant) is x = (quotient * y) + remainder and the remainder >= 0
+# The (post condition/ loop invariant) is used to check for correctness
+
+
+print( " x = 25 and y = 3 " + "then quotient and remainder are respectively "+str(div(25,3)))
