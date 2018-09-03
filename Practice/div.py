09774c64510a2eb5a6799ea973b3573a4b9c183d
Darius <darius.jones1018@gmail.com>
2018-09-02 17:23:14 -0700

    Added a summary

--- Practice/div.py
+++ Practice/div.py
@@ -11,19 +11,30 @@ def div(x,y):
 		raise ValueError("Invalid inputs for Division.")
 
 	#algorithm
-	quotient = 0
-	remainder = x
+	quotient = 0  #--- 1
+	remainder = x #---1
 
-	while y<= remainder:
-		remainder = remainder - y
-		quotient = quotient + 1
+	while y<=remainder: #--- n
+		remainder = remainder - y #---n
+		quotient = quotient + 1 #---n
 
-		print("remainder = " +str(remainder) + " quotient = "+ str(quotient))
+		print("remainder = " +str(remainder) + " quotient = "+ str(quotient)) #---n
 		
-	return quotient, remainder
+	return quotient, remainder #---1
+
+	#runtime is f(n) = 4n+3, O(n)
 
 # As per the book the (post condition/ loop invariant) is x = (quotient * y) + remainder and the remainder >= 0
 # The (post condition/ loop invariant) is used to check for correctness
+
+
+# We need to prove this by induction  x = (quotient * y) + remainder and the remainder >= 0
+# Base case: (before the loop starts) quotient = 0, remainder = x, so we have 
+# x = (0 * y) + r which is true.
+# Inductive step we one more iteration x = [(qoutient + 1) * y] + remainder - y, which holds true.  
+# So far we have proved partial correctness, we must show termination to show full correctness.
+# By LNP (Least Number Principle) the remainder is being reducded by y every iteration, therfore the loop
+# condition y<=remainder will eventually not hold. Full correctness has been shown.
 
 
 print( " x = 25 and y = 3 " + "then quotient and remainder are respectively "+str(div(25,3)))
