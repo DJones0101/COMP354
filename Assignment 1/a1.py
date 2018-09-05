62649a237bd42e69b7d2ea6719ad348876053866
Darius <darius.jones1018@gmail.com>
2018-09-05 11:20:11 -0700

    added to a1.

--- Assignment 1/a1.py
+++ Assignment 1/a1.py
@@ -1,18 +1,31 @@
-'''
-Darius Jones
-8/29/2018
-COMP 354
 
-'''
-
-def xgcd(m, n):
-
-	while n > 0:
-		(m, n) = (n, m % n)
-	return m
+# Darius Jones
+# 8/29/2018
+# COMP 354
 
 
+def xgcd(a, b):
 
+	#check precondtions
+	if a<=0 and b<=0 or a != int(a) or b != int(b):
+		raise ValueError("Invalid inputs")
+
+	m = int(a)
+	n = int(b)
+	r = m % n 
+
+	while r > 0:
+		q = m/n
+		m = n
+		n = r
+		r = m % n
+
+		
+	return n
+
+gcd = xgcd(56,15)
+
+print( " GCD of 56 and 15 is " + str(gcd))
 
 
 
