ae159fb1fc57f088c53dbbe5cb528c2768c31bd0
Darius <darius.jones1018@gmail.com>
2018-09-17 13:35:07 -0700

    Added comments on how to run the experiment on a1.py.

--- Assignment 1/a1.py
+++ Assignment 1/a1.py
@@ -5,6 +5,11 @@
 # Python 3.6.5
 
 
+# The runtime depends binary encoding of a. the runtime polynomial is 8n + 4, where n = 2^log2(|a| binary len). To run the expiriment
+# We run the algo on a group of numbers, calculate the runtime using the polynomial and draw a conclusion based on the length of the 
+# their binary strings.
+
+
 import math, sys
 
 def xgcd(b, a):
@@ -14,7 +19,7 @@ def xgcd(b, a):
     y = 0
     last_x = 0
     last_y = 1
-    run = 4
+    
 
 
     while a > 0:
@@ -22,9 +27,8 @@ def xgcd(b, a):
         q, b, a = math.floor(b/a), a, (b % a)
         x, last_x = last_x, x - q * last_x
         y, last_y = last_y, y - q * last_y
-        run += 4
-        
-        
+       
+         
 
         #print("a = " + str(a) +" b = " + str(b) + " x = " + str(x) + " last_x = " + str(last_x) + " y = " + str(y) + " last_y = " + str(last_y))
     run = run + 1
