26f7e5a1aa6cf75a23bf4dfde5f95c40093cac30
Darius <darius.jones1018@gmail.com>
2018-09-16 20:09:56 -0700

    moved things

--- Assignment 1/a1.py
+++ Assignment 1/a1.py
@@ -14,17 +14,21 @@ def xgcd(b, a):
     y = 0
     last_x = 0
     last_y = 1
+    run = 4
 
 
-    while a != 0:
+    while a > 0:
 
         q, b, a = math.floor(b/a), a, (b % a)
         x, last_x = last_x, x - q * last_x
         y, last_y = last_y, y - q * last_y
+        run += 4
+        
         
 
-        print("a = " + str(a) +" b = " + str(b) + " x = " + str(x) + " last_x = " + str(last_x) + " y = " + str(y) + " last_y = " + str(last_y))
-       
+        #print("a = " + str(a) +" b = " + str(b) + " x = " + str(x) + " last_x = " + str(last_x) + " y = " + str(y) + " last_y = " + str(last_y))
+    run = run + 1
+    print("runtime = " + str(run))
     return  b, x, y
 
 a = 56
