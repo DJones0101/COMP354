0b30bbae8f3cfbe0682ed5209aa697fcc639dbbd
Darius <darius.jones1018@gmail.com>
2018-09-16 19:54:01 -0700

    moved things

--- Assignment 1/a1.py
+++ Assignment 1/a1.py
@@ -2,27 +2,29 @@
 # Darius Jones
 # 9/9/2018
 # COMP 354
-# 1.2
+# Python 3.6.5
 
 
+import math, sys
 
 def xgcd(b, a):
 
-    #x0, last_x, y, last_y = 1, 0, 0, 1
 
     x = 1 
     y = 0
     last_x = 0
     last_y = 1
 
+
     while a != 0:
 
-        q, b, a = b // a, a, b % a
+        q, b, a = math.floor(b/a), a, (b % a)
         x, last_x = last_x, x - q * last_x
         y, last_y = last_y, y - q * last_y
+        
 
-        #print("a = " + str(a) +" b = " + str(b) + " x = " + str(x) + " last_x = " + str(last_x) + " y = " + str(y) + " last_y = " + str(last_y))
-
+        print("a = " + str(a) +" b = " + str(b) + " x = " + str(x) + " last_x = " + str(last_x) + " y = " + str(y) + " last_y = " + str(last_y))
+       
     return  b, x, y
 
 a = 56
