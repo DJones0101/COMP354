2002b580829fa321b0adaedd24b4d57d406e5c76
Darius <darius.jones1018@gmail.com>
2018-09-19 13:30:59 -0700

    added some comments to a1.py

--- p/gs.py
+++ p/gs.py
@@ -1,51 +1,11 @@
-## Darius Jones
-## Comp354
-## 9/12/2018
+# Darius Jones
+# Comp 354
+# 9/12/2018
+# Gale-Shapley alogrithm
 
 
-men = ['david','todd','kaleb']
-women = ['yumi' , 'savannah' , 'kim']
 
-pref = {
-	'david' : [ 'yumi' , 'savannah' , 'kim' ],
-	'todd' : [ 'savannah' , 'yumi' ,  'kim'],
-	'kaleb' : [ 'yumi' , 'savannah' , 'kim'] ,
- 	'yumi' : ['todd' , 'david', 'kaleb'] ,
- 	'savannah' : [ 'david' , 'todd' , 'kaleb'] ,
- 	'kim' : [ 'david' , 'todd' , 'kaleb']
-}
 
-for n in pref:
-	pref[n].reverse()
-
-rank = { 'yumi' : {  'david' : 2 , 'todd' : 1 , 'kaleb' : 3 } ,
-		 'savannah' : { 'david' : 1 , 'todd' : 2 , 'kaleb' : 3 } ,
-         'kim' : {  'david' : 1 , 'todd' : 2 , 'kaleb' : 3 }
-         }
-
-freemen = list(men)
-numpartners = len(men)
-S = {}
-
-while freemen:
-
-	m = freemen.pop()
-	#if len(pref[m]) == 0:
-		#continue
-
-	w = pref[m].pop()
-
-	if w not in S:
-		S[w] = m
-	else:
-		mprime = S[w]
-		if rank[w][m] < rank[w][mprime]:
-			S[w] = m
-			freemen.append(mprime)
-		else:
-			freemen.append(m)
-
-print(S)
 
 
 
