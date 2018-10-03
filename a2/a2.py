de38e5c295ae73cb62a393daaba06635162dbf98
Darius <darius.jones1018@gmail.com>
2018-10-03 11:25:45 -0700

    added comments to a2, fixed random key problem in prims

--- a2/a2.py
+++ a2/a2.py
@@ -5,7 +5,10 @@
 # COMP 354
 # Python 3.6
 
-import heapq as hq, networkx as nx, matplotlib.pyplot as plt
+import heapq as hq 
+import networkx as nx 
+import matplotlib.pyplot as plt
+import random
 from collections import defaultdict
 
 
@@ -16,9 +19,10 @@ graph = {
 	"d":{"a":5, "b":9,},
 }
 
-def prims(graph, start):
+def prims(graph):
 	 
 	T = defaultdict(set)
+	start = random.choice(list(graph)) # random key from dictionary
 	visited = set([start])
 	edges = [(weight, start, to) for to, weight, in graph[start].items()]
 	hq.heapify(edges)
@@ -39,13 +43,27 @@ def prims(graph, start):
 def show_graph(graph):
 
 	G = nx.Graph(graph)
-	pos = nx.circular_layout(G)
+	pos = nx.spring_layout(G)
 	labels = nx.get_node_attributes(G,"pos")
 	nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
 	nx.draw(G, with_labels=True)
 	plt.draw()
 	plt.show()
 
+def show_path(graph, mst):
+	G = nx.Graph(graph)
+	    
+	pos = nx.spring_layout(G)  #setting the positions with respect to G, not k.
+	k = G.subgraph(mst)  
+	pos = nx.spring_layout(G)
+	labels = nx.get_node_attributes(G,"pos")
+	nx.draw_networkx_edge_labels(k,pos,edge_labels=labels)
+	nx.draw(k, with_labels=True)
+	plt.draw()
+	plt.show()
+	
+
+#https://stackoverflow.com/questions/29838746/how-to-draw-subgraph-using-networkx  show subgrap
 
 # The goal is to color the edges after the algorithm was ran
 # https://stackoverflow.com/questions/25639169/networkx-change-color-width-according-to-edge-attributes-inconsistent-result
@@ -53,9 +71,11 @@ def show_graph(graph):
 
 def main():
 
-	mst = prims(graph, "a")
-	show_graph(graph)
-	show_graph(mst)
+	#show_graph(graph)
+	mst = prims(graph)
+	#show_path(graph, mst)
+	print (mst)
+
 
 
 
