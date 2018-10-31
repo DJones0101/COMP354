66d9cd5b2fc94bce1a93814fc9ae3fe00d7ec761
Darius <darius.jones1018@gmail.com>
2018-10-30 18:01:39 -0700

    small bugs in LMS, case3.

--- p/ch4/Lms/lms.py
+++ p/ch4/Lms/lms.py
@@ -3,97 +3,71 @@
 # Longest Monotone Subsequnce
 
 
-
 def lms(sequnce):
 		
-	result = dict()
-	result[0] = 1
+	R = dict()
+	R[0] = 1
 	length = len(sequnce)
 
+	for j in range(length):
+		maximum = 0
+		for i in range(j):
+			if R[i] > maximum and (sequnce[i] <= sequnce[j]):
+				maximum = R[i]
+		R[j] = (maximum + 1)
 
-	for index_j in range(length):
-		maxCount = 0
-		for index_i in range(index_j):
-			if result[index_i] > maxCount and (sequnce[index_i] <= sequnce[index_j]):
-				maxCount = result[index_i]
-
-		result[index_j] = (maxCount + 1)
-
-	 
-	return result
+	return R
 
 def buildSubsequnce(sequnce):
+	''' Takes in the sequnce (list), and returns a LMS list '''
 
 	#this holds the array of results
-	result =  lms(sequnce)
-	#this is the max longest sequnce
-	longestSeq = max(result,key =result.get)
+	R = lms(sequnce)
+	# (index, length)
+	values = [(index, length) for (index, length) in R.items()]
+	print(f"The values : {values}")
+	print(f"The sequnce : {sequnce}")
 
-	# The lms dictonary returns index : sequence length for that value, the index is
-	# the index of the original list of values. If i reverse the the ordering to sequnce : index 
-	# I can easily build the sequnce of values.
+	results = []
+	oldlenght = 0
+	for i in range(len(values)):
+		index, lenght = max(values, key=lambda x : x[1])
+		if oldlenght != lenght and sequnce[index] not in results:
+			print(f"The max value is {sequnce[index]}, The max lenght is {lenght}")
+			results.append(sequnce[index])
+			oldlenght = lenght
+		values.remove((index,lenght))
 
-	result1 = dict()
-	
-	for index, length in result.items():
-		result1[length] = index
+	results = results[::-1]
+	print(f"The results : {results}")
+	return results
 
-	toReturn = []
-	for length, index in list(result1.items())[::-1]:
-		if longestSeq == length and :
-			toReturn.append(sequnce[index])
-			longestSeq -= 1
-
-
-	return toReturn
-
-
-
-		
-
-
-		
-
-
-
-
-
-	
 
 
 def main():
 
-	a1 = [4,6,5,9,1]
-	#retval = lms(a1)
-	a2 = buildSubsequnce(a1)
-	print(a2)
-
-	# print(a1)
-	# for step, index in retval.items():
-	# 	print("%d : %d " %(step,index))
-	# print("-" * 50)
-
-	# a2 = [15,27,14,38,26,55,46,65,85,]
-	# retval1 = lms(a2)
-	# print(a2)
-	# for step, index in retval1.items():
-	# 	print("%d : %d " %(step,index))
-
+	# seq1 = [4,6,5,9,1,]
+	# ar1 = buildSubsequnce(seq1)
+	# print(f"The sequnce : {seq1}, the LMS : {ar1}")
 
 	# print("-" * 50)
-	# a3 = [7,6,1,4,7,8,20]
-	# retval2 = lms(a3)
-	# print(a3)
-	# for step, index in retval2.items():
-	# 	print("%d : %d " %(step,index))
+	# print("\n")
+	# seq2 = [15,27,14,38,26,55,46,65,85,]
+	# ar2 = buildSubsequnce(seq2)
+	# print(f"The sequnce : {seq2}, the LMS : {ar2}")
+
+
+	print("-" * 50)
+	print("\n")
+	seq3 = [7,6,1,4,7,8,20,]
+	ar3 = buildSubsequnce(seq3)
+	print(f"The sequnce : {seq3}, the LMS : {ar3}")
 
 	# print("-" * 50)
-	# a4 = [23,4,56,7,-3]
-	# retval2 = lms(a4)
-	# print(a4)
-	# for step, index in retval2.items():
-	# 	print("%d : %d " %(step,index))
-	#print(retval)
+	# print("\n")
+	# seq4 = [23,4,56,7,-3]
+	# ar4 = buildSubsequnce(seq4)
+	# print(f"The sequnce : {seq4}, the LMS : {ar4}")
 
 
 	
